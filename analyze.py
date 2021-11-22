2f378fe9e80bfedcab6b7df1a0a403c8b50a8b8a
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2021-11-22 22:41:53 +0300

    analyze script

---
+++ analyze.py
@@ -0,0 +1,20 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+csv = pd.read_csv(sys.argv[1])
+data = csv.values[1000:,1] / 1e6
+diffs_ts = np.array([*map(lambda x: (x[0], (x[1]-x[0])), zip(data[:-1], data[1:]))])
+diffs = diffs_ts[:,1]
+
+plt.figure(figsize=(12,3))
+plt.title("Time elapsed from previous gyro sample")
+plt.plot(diffs_ts[:,0], diffs_ts[:,1])
+plt.show()
+
+plt.figure(figsize=(12,3))
+plt.title("Logarithmic histogram of inter-sample delay")
+plt.hist(diffs, bins=100, log=True)
+np.percentile(diffs, 99)
+plt.show()
