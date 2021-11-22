91eee4edf8edec9d652e7222971e591e90797d5a
Vladimir Pinchuk <vladimir.pinchuk01@gmail.com>
2021-11-22 22:18:01 +0300

    experiment: remove isolated samples

--- resampler.py
+++ resampler.py
@@ -1,4 +1,5 @@
 import warnings
+import itertools
 import numpy as np
 from sklearn import linear_model
 from sklearn.exceptions import ConvergenceWarning
@@ -17,13 +18,43 @@ def gen_basis_cos(t, state_size):
   return basis
 
 def proc_axis(hawk, progress_callback):
-  alpha = 1e-3
+  # cleanup
+  clusters = [[0]]
+  critical_dt = 3e-3
+  min_cluster_length = 8
+  for i in range(1,len(hawk[0])):
+    if hawk[0][i] - hawk[0][i-1] > critical_dt:
+      clusters.append([])
+    clusters[-1].append(i)
+  total_clusters = len(clusters)
+  clusters = list(filter(lambda x: len(x) > min_cluster_length, clusters))
+  good_samples = list(itertools.chain.from_iterable(clusters))
+
+  print('Clusters discarded ', int(100 - len(clusters) / total_clusters * 100), '%')
+
+  # find very long gaps
+  very_long_gap_dt = 40e-3
+  very_long_gaps = []
+  if len(clusters) > 1:
+    for a,b in zip(clusters[:-1], clusters[1:]):
+      end, start = hawk[0][b[0]], hawk[0][a[-1]]
+      if end - start > very_long_gap_dt:
+        very_long_gaps.append((start, end))
+
+  print('Very long gaps / total ', len(very_long_gaps), len(clusters) - 1) 
+
+  hawk = hawk[:, good_samples]
+
+  # predict
+  alpha = 5e-4
   state_size = 500
+  dynamic_window = False
   window_size = .1
+  window_size_s = 100
   window = []
   new_smpl_dt = 1e-3
   new_data = [[0],[0]]
-  samples_per_window = 7
+  samples_per_window = 21
   lasso = linear_model.Lasso(alpha, copy_X = False, warm_start = True, tol = 1e-3, max_iter = 100)
 
   cc = 0
@@ -35,11 +66,11 @@ def proc_axis(hawk, progress_callback):
     window.append((time, basis, value))
     
     # drop old data from window
-    if len(window) >= 2:
-      i = 0
-      while window[-1][0] - window[i + 1][0] > window_size + new_smpl_dt:
-        i += 1
-      window = window[i:]
+    if len(window) > window_size_s:
+      window = window[-window_size_s:]
+    
+    if dynamic_window:
+      window_size = window[-1][0] - window[0][0]
 
     # generate new samples
     while new_data[0][-1] + new_smpl_dt < time - window_size / 2:
@@ -65,7 +96,20 @@ def proc_axis(hawk, progress_callback):
     if cc % 1024 == 0:
       progress_callback((time - hawk[0][0]) / (hawk[0][-1] - hawk[0][0]) * 100)
     cc += 1
-  return np.array(new_data, dtype = np.float64)
+  
+  new_data = np.array(new_data, dtype = np.float64)
+
+  # avg_window_len = 15
+  # avg_margin = 5
+  # for start, end in very_long_gaps:
+  #   start_i, end_i = int(start / new_smpl_dt + .5), int(end / new_smpl_dt + .5)
+  #   mean_start = np.mean(new_data[1, start_i - avg_window_len:start_i - avg_margin])
+  #   mean_end = np.mean(new_data[1, end_i + avg_margin:end_i + avg_window_len])
+  #   for i in range(start_i + 1, end_i):
+  #     pos = (i - start_i) / (end_i - start_i)
+  #     new_data[1, i] = pos * mean_end + (1 - pos) * mean_start
+
+  return new_data
 
 def proc_all(hawk_data):
   old_x = 0
